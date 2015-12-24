80e42f9277815c27a88940162155f9cb3171bcb2
Zematus <jb_tamez@hotmail.com>
2015-12-24 17:35:17 -0600

    UI changes and Time control base

---
+++ Assets/Scripts/2D/ToggleButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ToggleButtonScript : MonoBehaviour {
+
+	public Image CheckImage;
+	public Image UncheckImage;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnValueChanged (bool value) {
+
+	}
+}
