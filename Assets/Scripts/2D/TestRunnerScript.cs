d72b6f3c0bb3d86b71e17f1a31cdacff3e2a2369
Zematus <jb_tamez@hotmail.com>
2016-06-19 01:24:30 -0700

    Scene for Test Automation

---
+++ Assets/Scripts/2D/TestRunnerScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestRunnerScript : MonoBehaviour {
+
+	public AutomatedTest[] tests = new AutomatedTest[] {
+		new SaveLoadTest ()
+	};
+
+	private int _prevTestIndex = -1;
+	private int _testIndex = 0;
+
+	private int _successes = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log ("Running Tests...\n");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Manager.ExecuteTasks (100);
+
+		if (_testIndex == tests.Length) {
+
+			Debug.Log ("\nFinished Tests!");
+			Debug.Log (_successes + " of " + tests.Length + " Succeded");
+			Debug.Break ();
+
+		} else {
+
+			AutomatedTest test = tests [_testIndex];
+
+			if (_prevTestIndex != _testIndex) {
+				
+				Debug.Log ("Executing test: " + _testIndex);
+
+				_prevTestIndex = _testIndex;
+			}
+
+			test.Run ();
+
+			_successes += (test.State == TestState.Succeded) ? 1 : 0;
+
+			if ((test.State == TestState.Succeded) || (test.State == TestState.Failed))
+				_testIndex++;
+		}
+	}
+}
--- Assets/Tools/Scripts/AutomatedTest.cs
+++ Assets/Tools/Scripts/AutomatedTest.cs
@@ -14,9 +14,21 @@ public abstract class AutomatedTest {
 	public TestState State { get; protected set; }
 
 	public abstract void Run ();
+
+	protected AutomatedTest () {
+	
+		State = TestState.NotStarted;
+	}
 }
 
 public class SaveLoadTest : AutomatedTest {
+
+	public enum Stage {
+
+		Generation,
+		Save,
+		Load
+	}
 
 	private int _eventCountBeforeSave;
 	private int _eventCountAfterSave;
@@ -26,82 +38,147 @@ public class SaveLoadTest : AutomatedTest {
 
 	private bool _result = true;
 
+	private string _savePath;
+
+	private World _world;
+
+	private Stage _stage = Stage.Generation;
+
 	public SaveLoadTest () {
+		
 	}
 
 	public override void Run () {
 
-		string path = Manager.SavePath + "TestSaveLoad.plnt";
+		switch (_stage) {
 
-		Manager.GenerateNewWorld (407252633);
+		case Stage.Generation:
+			
+			if (State == TestState.NotStarted) {
 
-		int population = (int)Mathf.Ceil (World.StartPopulationDensity * TerrainCell.MaxArea);
+				Manager.LoadAppSettings (@"Worlds.settings");
 
-		Manager.GenerateRandomHumanGroup (population);
+				Manager.UpdateMainThreadReference ();
+			
+				_savePath = Manager.SavePath + "TestSaveLoad.plnt";
 
-		World world = Manager.CurrentWorld;
+				Manager.GenerateNewWorldAsync (407252633);
 
-		while (world.CurrentDate < 100) {
+				State = TestState.Running;
+			}
 
-			Manager.CurrentWorld.Iterate ();
+			if (!Manager.WorldReady) {
+
+				return;
+			}
+
+			if (Manager.PerformingAsyncTask) {
+				return;
+			}
+
+			int population = (int)Mathf.Ceil (World.StartPopulationDensity * TerrainCell.MaxArea);
+
+			Manager.GenerateRandomHumanGroup (population);
+
+			_world = Manager.CurrentWorld;
+
+			Debug.Log ("Pushing simulation forward at least 100 years...");
+
+			while (_world.CurrentDate < 100) {
+
+				Manager.CurrentWorld.Iterate ();
+			}
+
+			_saveDate = _world.CurrentDate;
+
+			Debug.Log ("Save Date: " + _saveDate);
+
+			_eventCountBeforeSave = _world.EventsToHappenCount;
+			Debug.Log ("Number of Events before save: " + _eventCountBeforeSave);
+
+			Manager.SaveWorldAsync (_savePath);
+
+			_stage = Stage.Save;
+
+			break;
+
+		case Stage.Save:
+			
+			if (Manager.PerformingAsyncTask) {
+				return;
+			}
+
+			_eventCountAfterSave = _world.EventsToHappen.Count;
+			Debug.Log ("Number of Events after save: " + _eventCountAfterSave);
+
+			if (_eventCountBeforeSave != _eventCountAfterSave) {
+
+				Debug.LogError ("Number of events before and after save are different");
+
+				_result = false;
+
+			} else {
+
+				Debug.Log ("Number of Events remain equal after save");
+			}
+
+			Debug.Log ("Pushing simulation forward...");
+
+			while (_world.CurrentDate < (_saveDate + 100)) {
+
+				Manager.CurrentWorld.Iterate ();
+			}
+
+			Debug.Log ("Current Date: " + _world.CurrentDate);
+
+			Manager.LoadWorldAsync (_savePath);
+
+			_stage = Stage.Load;
+
+			break;
+
+		case Stage.Load:
+
+			if (Manager.PerformingAsyncTask) {
+				return;
+			}
+
+			_world = Manager.CurrentWorld;
+
+			Debug.Log ("Date after Load: " + _world.CurrentDate);
+			
+			_eventCountAfterLoad = _world.EventsToHappen.Count;
+			Debug.Log ("Number of Events after load: " + _eventCountAfterLoad);
+
+			if (_eventCountAfterLoad != _eventCountAfterSave) {
+
+				Debug.LogError ("Number of events after load different from after save");
+
+				_result = false;
+
+			} else {
+
+				Debug.Log ("Number of Events remain equal after load");
+			}
+
+			Debug.Log ("Pushing simulation forward...");
+
+			while (_world.CurrentDate < (_saveDate + 100)) {
+
+				Manager.CurrentWorld.Iterate ();
+			}
+
+			Debug.Log ("Current Date: " + _world.CurrentDate);
+
+			if (_result)
+				State = TestState.Succeded;
+			else
+				State = TestState.Failed;
+
+			break;
+
+		default:
+			throw new System.Exception ("Unrecognized test stage: " + _stage);
 		}
-
-		_saveDate = world.CurrentDate;
-
-		Debug.Log ("Save Date: " + _saveDate);
-
-		_eventCountBeforeSave = world.EventsToHappenCount;
-		Debug.Log ("Number of Events before save: " + _eventCountBeforeSave);
-
-		Manager.SaveWorld (path);
-
-		_eventCountAfterSave = world.EventsToHappen.Count;
-		Debug.Log ("Number of Events after save: " + _eventCountAfterSave);
-
-		if (_eventCountBeforeSave != _eventCountAfterSave) {
-
-			Debug.LogError ("Number of events before and after save are different");
-
-			_result = false;
-
-		} else {
-
-			Debug.Log ("Number of Events remain equal after save");
-		}
-
-		while (world.CurrentDate == _saveDate) {
-
-			Manager.CurrentWorld.Iterate ();
-		}
-
-		Debug.Log ("Current Date: " + world.CurrentDate);
-
-		Manager.LoadWorld (path);
-
-		_eventCountAfterLoad = world.EventsToHappen.Count;
-		Debug.Log ("Number of Events after load: " + _eventCountAfterLoad);
-
-		if (_eventCountAfterLoad != _eventCountAfterSave) {
-
-			Debug.LogError ("Number of events after load different from after save");
-
-			_result = false;
-
-		} else {
-
-			Debug.Log ("Number of Events remain equal after load");
-		}
-
-		if (world.CurrentDate == _saveDate) {
-
-			Manager.CurrentWorld.Iterate ();
-		}
-
-		Debug.Log ("Current Date: " + world.CurrentDate);
-
-		if (_result)
-			State = TestState.Succeded;
-		else
-			State = TestState.Failed;
 	}
 }
