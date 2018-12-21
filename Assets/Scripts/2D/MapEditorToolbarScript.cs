3898fcbd8ae4cb50da50520312ff1d8013755a44
Zematus <Zematus@users.noreply.github.com>
2018-12-20 20:48:06 -0600

    Added and configured basic map editor toolbar slider panes

---
+++ Assets/Scripts/2D/MapEditorToolbarScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class MapEditorToolbarScript : MonoBehaviour
+{
+    public SliderControlsScript AltitudeScaleSliderControlsScript;
+    public SliderControlsScript SeaLevelSliderControlsScript;
+    public SliderControlsScript TempLevelSliderControlsScript;
+    public SliderControlsScript RainLevelSliderControlsScript;
+
+    private const float _minAltitudeScaleOffset = 0;
+    private const float _maxAltitudeScaleOffset = 100;
+    private const float _defaultAltitudeScaleOffset = 100;
+
+    private const float _minSeaLevelOffset = -10000;
+    private const float _maxSeaLevelOffset = 10000;
+    private const float _defaultSeaLevelOffset = 0;
+
+    private const float _minTemperatureOffset = -50 + World.AvgPossibleTemperature;
+    private const float _maxTemperatureOffset = 50 + World.AvgPossibleTemperature;
+    private const float _defaultTemperatureOffset = World.AvgPossibleTemperature;
+
+    private const float _minRainfallOffset = -800f + World.AvgPossibleRainfall;
+    private const float _maxRainfallOffset = 800f + World.AvgPossibleRainfall;
+    private const float _defaultRainfallOffset = World.AvgPossibleRainfall;
+
+    // Use this for initialization
+    void Start()
+    {
+        AltitudeScaleSliderControlsScript.MinValue = _minAltitudeScaleOffset;
+        AltitudeScaleSliderControlsScript.MaxValue = _maxAltitudeScaleOffset;
+        AltitudeScaleSliderControlsScript.DefaultValue = _defaultAltitudeScaleOffset;
+        AltitudeScaleSliderControlsScript.Initialize();
+
+        SeaLevelSliderControlsScript.MinValue = _minSeaLevelOffset;
+        SeaLevelSliderControlsScript.MaxValue = _maxSeaLevelOffset;
+        SeaLevelSliderControlsScript.DefaultValue = _defaultSeaLevelOffset;
+        SeaLevelSliderControlsScript.Initialize();
+
+        TempLevelSliderControlsScript.MinValue = _minTemperatureOffset;
+        TempLevelSliderControlsScript.MaxValue = _maxTemperatureOffset;
+        TempLevelSliderControlsScript.DefaultValue = _defaultTemperatureOffset;
+        TempLevelSliderControlsScript.Initialize();
+
+        RainLevelSliderControlsScript.MinValue = _minRainfallOffset;
+        RainLevelSliderControlsScript.MaxValue = _maxRainfallOffset;
+        RainLevelSliderControlsScript.DefaultValue = _defaultRainfallOffset;
+        RainLevelSliderControlsScript.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/2D/SliderControlsScript.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class SliderControlsScript : MonoBehaviour
+{
+    public Button ResetButton;
+    public Slider Slider;
+    public InputField InputField;
+    public Text SymbolText;
+
+    public string InputFormat = "0.000";
+    public string UnitSymbol = "%";
+
+    public float MinValue = 0;
+    public float MaxValue = 100;
+    public float DefaultValue = 100;
+
+    public float CurrentValue = 100;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Initialize()
+    {
+        Slider.minValue = MinValue;
+        Slider.maxValue = MaxValue;
+
+        SymbolText.text = UnitSymbol;
+
+        Reset();
+    }
+
+    public void SetValueFromSlider(System.Single value)
+    {
+        SetValue(value);
+    }
+
+    public void SetValueFromInputField(string valueStr)
+    {
+        float value = 0;
+
+        float.TryParse(valueStr, out value);
+
+        SetValue(value);
+    }
+
+    public void SetValue(float value)
+    {
+        if (value < MinValue)
+            value = MinValue;
+
+        if (value > MaxValue)
+            value = MaxValue;
+
+        CurrentValue = value;
+
+        Slider.value = value;
+
+        InputField.text = value.ToString(InputFormat);
+    }
+
+    public void Reset()
+    {
+        SetValue(DefaultValue);
+    }
+}
