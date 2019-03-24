dd683ceb9b6f8bea31b6e5cdad4cda32b92d8060
Zematus <jb_tamez@hotmail.com>
2019-03-23 19:22:01 -0700

    Fixed and tweaked sphere surface dragging

--- Assets/Scripts/3D/PlanetScript.cs
+++ Assets/Scripts/3D/PlanetScript.cs
@@ -7,9 +7,14 @@ public class PlanetScript : MonoBehaviour
     public Camera Camera;
     public SphereCollider SphereCollider;
 
+    public GameObject Pivot;
+
     private bool _isDraggingSurface = false;
 
     private Vector3 _sphereRotateLastDragPosition;
+    private Vector3 _initialDragMousePosition;
+
+    private const float _rayDistFactor = 2.5f;
 
     // Update is called once per frame
 
@@ -56,25 +61,51 @@ public class PlanetScript : MonoBehaviour
 
         Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
 
-        RaycastHit raycastHit;
+        float distance = (Camera.transform.position - transform.position).magnitude * _rayDistFactor - Camera.nearClipPlane;
 
-        if (!SphereCollider.Raycast(ray, out raycastHit, 50))
-        {
-            return;
-        }
-        
-        Vector3 currentDragPosition = raycastHit.point - transform.position;
+        Vector3 currentDragPosition = ray.GetPoint(distance);
+
+        Vector3 pivotLastDragPosition = Pivot.transform.InverseTransformVector(_sphereRotateLastDragPosition);
+        Vector3 pivotCurrentDragPosition = Pivot.transform.InverseTransformVector(currentDragPosition);
 
         Vector3 localLastDragPosition = transform.InverseTransformVector(_sphereRotateLastDragPosition);
         Vector3 localCurrentDragPosition = transform.InverseTransformVector(currentDragPosition);
 
-        float angleX = Vector3.SignedAngle(localLastDragPosition, localCurrentDragPosition, Vector3.right);
+        float angleXPivot = Vector3.SignedAngle(pivotLastDragPosition, pivotCurrentDragPosition, Vector3.right);
         float angleY = Vector3.SignedAngle(localLastDragPosition, localCurrentDragPosition, Vector3.up);
-        float angleZ = Vector3.SignedAngle(localLastDragPosition, localCurrentDragPosition, Vector3.forward);
 
-        transform.Rotate(angleX, 0, 0);
+        float absDX = Mathf.Max(0.001f, Mathf.Abs(_initialDragMousePosition.x - mousePosition.x));
+        float absDY = Mathf.Max(0.001f, Mathf.Abs(_initialDragMousePosition.y - mousePosition.y));
+
+        // Introduce a rotation bias toward a single axis to minimize rotation wobble
+        angleXPivot *= Mathf.Min(1, absDY / absDX);
+        angleY *= Mathf.Min(1, absDX / absDY);
+
+        Pivot.transform.Rotate(angleXPivot, 0, 0);
+        transform.Rotate(0, angleY, 0);
+
+        Quaternion pivotRotation = Pivot.transform.rotation;
+        Vector3 pivotEulerAngles = pivotRotation.eulerAngles;
+        
+        // Prevent the globe pivot from rotating beyond the poles
+        if ((pivotEulerAngles.x > 89) && (pivotEulerAngles.x <= 135))
+        {
+            pivotEulerAngles.x = 89;
+        }
+        if ((pivotEulerAngles.x < 271) && (pivotEulerAngles.x > 135))
+        {
+            pivotEulerAngles.x = 271;
+        }
+        pivotEulerAngles.y = 0;
+        pivotEulerAngles.z = 0;
+
+        pivotRotation.eulerAngles = pivotEulerAngles;
+        Pivot.transform.rotation = pivotRotation;
 
         _sphereRotateLastDragPosition = currentDragPosition;
+
+        // Lerp initial mouse pos toward current mouse pos to soften axis switching in rotation bias
+        _initialDragMousePosition = Vector3.Lerp(_initialDragMousePosition, mousePosition, 0.25f);
     }
 
     private void BeginDragSurface(Vector3 mousePosition)
@@ -90,7 +121,11 @@ public class PlanetScript : MonoBehaviour
             return; // Didn't actually pressed mouse on the sphere's surface
         }
 
-        _sphereRotateLastDragPosition = raycastHit.point - transform.position;
+        _initialDragMousePosition = mousePosition;
+
+        float distance = (Camera.transform.position - transform.position).magnitude * _rayDistFactor - Camera.nearClipPlane;
+
+        _sphereRotateLastDragPosition = ray.GetPoint(distance);
 
         _isDraggingSurface = true;
 
