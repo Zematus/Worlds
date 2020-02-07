b85119f95ba8987e13190441b2fe6959995d3994
Juan Bernardo Tamez Pena <jb_tamez@hotmail.com>
2020-02-06 21:32:35 -0800

    Generalizing string expressions

---
+++ Assets/Scripts/WorldEngine/Modding/Expressions/StringExpressions/FixedStringValueExpression.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class FixedStringValueExpression : StringExpression
+{
+    public const string Regex = @"^" + ModUtility.IdentifierRegexPart + @"\s*$";
+
+    public string StringValue;
+
+    public FixedStringValueExpression(string identifier)
+    {
+        StringValue = identifier;
+    }
+
+    protected override string Evaluate()
+    {
+        return StringValue;
+    }
+
+    public override string GetValue()
+    {
+        return StringValue;
+    }
+
+    public override string ToString()
+    {
+        return StringValue.ToString();
+    }
+}
