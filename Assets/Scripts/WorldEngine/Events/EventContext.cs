1d2a5a5069a75fa0587d717e8dead3636394b5e3
Juan Bernardo Tamez Pena <jb_tamez@hotmail.com>
2020-02-19 22:26:46 -0800

    Documenting work so far - WIP

--- Assets/Scripts/WorldEngine/Events/EventContext.cs
+++ Assets/Scripts/WorldEngine/Events/EventContext.cs
@@ -3,18 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
+/// <summary>
+/// Defines a context for event associated expressions to use
+/// </summary>
 public class EventContext : Context
 {
     public const string TargetId = "target";
 
+    /// <summary>
+    /// The target of the event to trigger
+    /// </summary>
     public Entity Target;
+    /// <summary>
+    /// The type of the event target
+    /// </summary>
     public string TargetType;
 
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="targetStr">String identifying the type of target</param>
     public EventContext(string targetStr)
     {
         SetTarget(targetStr);
     }
 
+    /// <summary>
+    /// Sets the target entity for the event based in the type of target
+    /// </summary>
+    /// <param name="targetStr"></param>
     private void SetTarget(string targetStr)
     {
         switch (targetStr)
@@ -31,6 +48,7 @@ public class EventContext : Context
 
         TargetType = targetStr;
 
+        // Add the target to the context's entity map
         Entities.Add(TargetId, Target);
     }
 }
--- Assets/Scripts/WorldEngine/Events/EventLoader.cs
+++ Assets/Scripts/WorldEngine/Events/EventLoader.cs
@@ -4,13 +4,24 @@ using System.Collections.Generic;
 using System;
 using System.IO;
 
+/// <summary>
+/// Class used to load event mod entries from mod JSON files.
+/// Class properties should match the root structure of the JSON file.
+/// </summary>
 [Serializable]
 public class EventLoader
 {
-#pragma warning disable 0649
+#pragma warning disable 0649 // Disable warning for unitialized properties...
 
+    /// <summary>
+    /// list of loaded event entries.
+    /// </summary>
     public LoadedEvent[] events;
 
+    /// <summary>
+    /// Object defininf an event entry. Structure must match that of
+    /// an event entry in the mod file
+    /// </summary>
     [Serializable]
     public class LoadedEvent
     {
@@ -25,22 +36,31 @@ public class EventLoader
 
 #pragma warning restore 0649
 
+    /// <summary>
+    /// Produce a set of Event Generator objects based on the contents an event mod file
+    /// </summary>
+    /// <param name="filename">Name of the JSON file with events to load</param>
+    /// <returns>A set of Event Generator objects,
+    /// each one relating to a single event entry on the file</returns>
     public static IEnumerable<EventGenerator> Load(string filename)
     {
         string jsonStr = File.ReadAllText(filename);
 
+        // Load json object from file into intermediary object
         EventLoader loader = JsonUtility.FromJson<EventLoader>(jsonStr);
 
         for (int i = 0; i < loader.events.Length; i++)
         {
-            EventGenerator generator = null;
-
+            EventGenerator generator;
             try
             {
                 generator = CreateEventGenerator(loader.events[i]);
             }
             catch (Exception e)
             {
+                // If theres a failure while loading an event entry. Report
+                // the file from which the event came from and its index within
+                // the file..
                 throw new Exception(
                     "Failure loading event #" + i + " in " + filename + ": "
                     + e.Message, e);
@@ -50,6 +70,11 @@ public class EventLoader
         }
     }
 
+    /// <summary>
+    /// Produces a generator object from a single event entry
+    /// </summary>
+    /// <param name="e">The event entry</param>
+    /// <returns>The resulting event generator</returns>
     private static EventGenerator CreateEventGenerator(LoadedEvent e)
     {
         if (string.IsNullOrEmpty(e.id))
@@ -77,6 +102,7 @@ public class EventLoader
             throw new ArgumentException("'effects' list can't be empty");
         }
 
+        // Generate a new event context object to use when evaluating expressions
         EventContext context = new EventContext(e.target);
 
         IBooleanExpression[] assignmentConditions = null;
@@ -86,21 +112,26 @@ public class EventLoader
 
         if (e.assignmentConditions != null)
         {
+            // Build the assignment condition expressions (must evaluate to bool values)
             assignmentConditions =
                 ExpressionBuilder.BuildBooleanExpressions(context, e.assignmentConditions);
         }
 
         if (e.triggerConditions != null)
         {
+            // Build the trigger condition expressions (must evaluate to bool values)
             triggerConditions =
                 ExpressionBuilder.BuildBooleanExpressions(context, e.triggerConditions);
         }
 
+        // Build the time-to-trigger expression (must evaluate to a number (int or float))
         timeToTrigger = ExpressionBuilder.ValidateNumericExpression(
             ExpressionBuilder.BuildExpression(context, e.timeToTrigger));
 
+        // Build the effect expressions (must produce side effects)
         effects = ExpressionBuilder.BuildEffectExpressions(context, e.effects);
 
+        // Buikd generator object and return it
         EventGenerator generator = new EventGenerator()
         {
             Context = context,
