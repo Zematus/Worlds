62f4255251781f65b4aec01983dca57dab460edf
Juan Bernardo Tamez Pena <jb_tamez@hotmail.com>
2020-09-24 08:28:13 -0700

    Revert "Refactored some attributes to do type validation on load"
    This reverts commit 0802100a3c54792bdbc1ce6293030b95d8e53820.

--- Assets/Scripts/WorldEngine/Modding/Entities/Attributes/Faction Entity Attributes/RelationshipAttribute.cs
+++ Assets/Scripts/WorldEngine/Modding/Entities/Attributes/Faction Entity Attributes/RelationshipAttribute.cs
@@ -7,32 +7,29 @@ public class RelationshipAttribute : ValueEntityAttribute<float>
 {
     private FactionEntity _factionEntity;
 
-    private readonly FactionEntity _targetFactionEntity;
+    private readonly IValueExpression<Entity> _argumentExp;
 
     public RelationshipAttribute(FactionEntity factionEntity, IExpression[] arguments)
         : base(FactionEntity.RelationshipAttributeId, factionEntity, arguments, 1)
     {
         _factionEntity = factionEntity;
 
-        IValueExpression<Entity> argumentExp =
-            ValueExpressionBuilder.ValidateValueExpression<Entity>(arguments[0]);
-
-        _targetFactionEntity = argumentExp.Value as FactionEntity;
-
-        if (_targetFactionEntity == null)
-        {
-            throw new System.Exception(
-                "Input parameter is not of a valid faction entity: " + argumentExp.Value.GetType() +
-                "\n - expression: " + argumentExp.ToString() +
-                "\n - value: " + argumentExp.ToPartiallyEvaluatedString());
-        }
+        _argumentExp = ValueExpressionBuilder.ValidateValueExpression<Entity>(arguments[0]);
     }
 
     public override float Value
     {
         get
         {
-            return _factionEntity.Faction.GetRelationshipValue(_targetFactionEntity.Faction);
+            if (_argumentExp.Value is FactionEntity fEntity)
+            {
+                return _factionEntity.Faction.GetRelationshipValue(fEntity.Faction);
+            }
+
+            throw new System.Exception(
+                "Input parameter is not of a valid faction entity: " + _argumentExp.Value.GetType() +
+                "\n - expression: " + _argumentExp.ToString() +
+                "\n - value: " + _argumentExp.ToPartiallyEvaluatedString());
         }
     }
 }
