f0e631911a30c82009acb562e313d59a9c15f178
Juan Bernardo Tamez Pena <jb_tamez@hotmail.com>
2020-09-24 08:28:13 -0700

    Revert "Refactored some attributes to do type validation on load"
    This reverts commit 0802100a3c54792bdbc1ce6293030b95d8e53820.

--- Assets/Scripts/WorldEngine/Modding/Entities/Attributes/Polity Entity Attributes/TransferInfluenceAttribute.cs
+++ Assets/Scripts/WorldEngine/Modding/Entities/Attributes/Polity Entity Attributes/TransferInfluenceAttribute.cs
@@ -7,47 +7,46 @@ public class TransferInfluenceAttribute : EffectEntityAttribute
 {
     private PolityEntity _polityEntity;
 
+    private readonly IValueExpression<Entity> _sourceFactionExp;
+    private readonly IValueExpression<Entity> _targetFactionExp;
     private readonly IValueExpression<float> _percentExp;
-
-    private readonly FactionEntity _sourceFactionEntity;
-    private readonly FactionEntity _targetFactionEntity;
 
     public TransferInfluenceAttribute(PolityEntity polityEntity, IExpression[] arguments)
         : base(PolityEntity.TransferInfluenceAttributeId, polityEntity, arguments, 3)
     {
         _polityEntity = polityEntity;
 
+        _sourceFactionExp = ValueExpressionBuilder.ValidateValueExpression<Entity>(arguments[0]);
+        _targetFactionExp = ValueExpressionBuilder.ValidateValueExpression<Entity>(arguments[1]);
         _percentExp = ValueExpressionBuilder.ValidateValueExpression<float>(arguments[2]);
-
-        IValueExpression<Entity> sourceFactionExp =
-            ValueExpressionBuilder.ValidateValueExpression<Entity>(arguments[0]);
-
-        _sourceFactionEntity = sourceFactionExp.Value as FactionEntity;
-
-        if (_sourceFactionEntity == null)
-        {
-            throw new System.Exception(
-                "Input parameter 0 is not of a valid faction entity: " + sourceFactionExp.Value.GetType() +
-                "\n - expression: " + sourceFactionExp.ToString() +
-                "\n - value: " + sourceFactionExp.ToPartiallyEvaluatedString());
-        }
-
-        IValueExpression<Entity> targetFactionExp =
-            ValueExpressionBuilder.ValidateValueExpression<Entity>(arguments[1]);
-
-        _targetFactionEntity = targetFactionExp.Value as FactionEntity;
-
-        if (_targetFactionEntity == null)
-        {
-            throw new System.Exception(
-                "Input parameter 1 is not of a valid faction entity: " + targetFactionExp.Value.GetType() +
-                "\n - expression: " + targetFactionExp.ToString() +
-                "\n - value: " + targetFactionExp.ToPartiallyEvaluatedString());
-        }
     }
 
     public override void Apply()
     {
+        FactionEntity sourceEntity = _sourceFactionExp.Value as FactionEntity;
+
+        if (sourceEntity == null)
+        {
+            throw new System.ArgumentException(
+                "transfer_influence: invalid source faction to set relationship to: " +
+                "\n - expression: " + ToString() +
+                "\n - source faction: " + _sourceFactionExp.ToPartiallyEvaluatedString() +
+                "\n - target faction: " + _targetFactionExp.ToPartiallyEvaluatedString() +
+                "\n - percentage expression: " + _percentExp.ToPartiallyEvaluatedString());
+        }
+
+        FactionEntity targetEntity = _targetFactionExp.Value as FactionEntity;
+
+        if (targetEntity == null)
+        {
+            throw new System.ArgumentException(
+                "transfer_influence: invalid target faction to set relationship to: " +
+                "\n - expression: " + ToString() +
+                "\n - source faction: " + _sourceFactionExp.ToPartiallyEvaluatedString() +
+                "\n - target faction: " + _targetFactionExp.ToPartiallyEvaluatedString() +
+                "\n - percentage expression: " + _percentExp.ToPartiallyEvaluatedString());
+        }
+
         float percentValue = _percentExp.Value;
 
         if (!percentValue.IsInsideRange(0, 1))
@@ -55,10 +54,12 @@ public class TransferInfluenceAttribute : EffectEntityAttribute
             throw new System.ArgumentException(
                 "transfer_influence: percentage to transfer outside of range (0, 1): " +
                 "\n - expression: " + ToString() +
+                "\n - source faction: " + _sourceFactionExp.ToPartiallyEvaluatedString() +
+                "\n - target faction: " + _targetFactionExp.ToPartiallyEvaluatedString() +
                 "\n - percentage expression: " + _percentExp.ToPartiallyEvaluatedString() +
                 "\n - percentage value: " + _percentExp.ToPartiallyEvaluatedString());
         }
 
-        Polity.TransferInfluence(_sourceFactionEntity.Faction, _targetFactionEntity.Faction, percentValue);
+        Polity.TransferInfluence(sourceEntity.Faction, targetEntity.Faction, percentValue);
     }
 }
