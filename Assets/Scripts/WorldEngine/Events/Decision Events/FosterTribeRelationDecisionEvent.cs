caa304174fdd54ff0d834992d387ad414a83e84c
Juan Bernardo Tamez Pena <jb_tamez@hotmail.com>
2020-09-27 13:17:04 -0700

    Disabled hardcoded foster relationship event

--- Assets/Scripts/WorldEngine/Events/Decision Events/FosterTribeRelationDecisionEvent.cs
+++ Assets/Scripts/WorldEngine/Events/Decision Events/FosterTribeRelationDecisionEvent.cs
@@ -5,6 +5,7 @@ using System.Xml;
 using System.Xml.Serialization;
 using UnityEngine.Profiling;
 
+[System.Obsolete]
 public class FosterTribeRelationDecisionEvent : PolityEvent
 {
     public const long DateSpanFactorConstant = CellGroup.GenerationSpan * 5;
