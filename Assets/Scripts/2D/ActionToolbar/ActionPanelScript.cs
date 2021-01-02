7b75860ce8bd719196c8408933bfe78f1c13c1a1
Zematus <jb_tamez@hotmail.com>
2021-01-01 16:28:32 -0800

    More toolbar work and tooltip improvements

---
+++ Assets/Scripts/2D/ActionToolbar/ActionPanelScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ActionPanelScript : MonoBehaviour
+{
+    public ActionButtonScript ActionButtonPrefab;
+
+    private HashSet<ActionButtonScript> _buttons = new HashSet<ActionButtonScript>();
+
+    public void AddActionButton()
+    {
+        ActionButtonScript button =
+            Instantiate(ActionButtonPrefab) as ActionButtonScript;
+
+        button.transform.SetParent(transform);
+        button.transform.localScale = Vector3.one;
+
+        _buttons.Add(button);
+    }
+
+    public void ClearActionButtons()
+    {
+        foreach (ActionButtonScript button in _buttons)
+        {
+            button.Remove();
+        }
+
+        _buttons.Clear();
+    }
+}
