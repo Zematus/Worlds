66df666b668b20f3d6be56003437d99b9d589714
Zematus <jb_tamez@hotmail.com>
2021-01-02 00:18:52 -0800

    More action toolbar work and small worldgen fix

---
+++ Assets/Scripts/2D/ActionToolbar/ActionToolbarScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public class ActionToolbarScript : MonoBehaviour
+{
+    public void SetVisible(bool state)
+    {
+        gameObject.SetActive(state);
+    }
+}
