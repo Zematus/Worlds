2ebcf0949fcca61faf0d9b484bb94eab6dd0fa68
Zematus <jb_tamez@hotmail.com>
2021-01-01 16:28:32 -0800

    More toolbar work and tooltip improvements

---
+++ Assets/Scripts/2D/ActionToolbar/ActionButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ActionButtonScript : MonoBehaviour
+{
+    public Text Text;
+
+    public void SetText(string text)
+    {
+        Text.text = text;
+    }
+
+    public void Remove()
+    {
+        gameObject.SetActive(false);
+
+        Destroy(gameObject);
+    }
+}
