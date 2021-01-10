6988031dcbb0a2c4241ca6d4e2a01fac1bcdb3f4
Zematus <jb_tamez@hotmail.com>
2021-01-10 01:04:01 -0800

    More action category behavior

---
+++ Assets/Scripts/2D/ActionToolbar/ActionCategoryScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ActionCategoryScript : MonoBehaviour
+{
+    public ActionPanelScript ActionPanel;
+
+    public Image ToogleImage;
+
+    public string TooltipText;
+
+    public TooltipHandlerScript TooltipHandler;
+
+    public void Remove()
+    {
+        gameObject.SetActive(false);
+
+        Destroy(gameObject);
+    }
+
+    public void UpdateTooltip()
+    {
+        TooltipHandler.SetText(TooltipText);
+    }
+}
