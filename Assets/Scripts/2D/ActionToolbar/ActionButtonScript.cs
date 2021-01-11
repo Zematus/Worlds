ee1a0fa46c02a1f453712cd2cb6661799f09f472
Zematus <jb_tamez@hotmail.com>
2021-01-11 00:28:32 -0800

    Added partial action button functionality

--- Assets/Scripts/2D/ActionToolbar/ActionButtonScript.cs
+++ Assets/Scripts/2D/ActionToolbar/ActionButtonScript.cs
@@ -6,13 +6,36 @@ public class ActionButtonScript : MonoBehaviour
 {
     public Text Text;
 
+    public Button Button;
+
     private Action _action;
+
+    private long _lastDate = -1;
+
+    public void Update()
+    {
+        if (!gameObject.activeInHierarchy)
+            return;
+
+        if (_lastDate == Manager.CurrentWorld.CurrentDate)
+            return;
+
+        _lastDate = Manager.CurrentWorld.CurrentDate;
+
+        _action.SetTarget(Manager.CurrentWorld.GuidedFaction);
+
+        Button.interactable = _action.CanUse();
+    }
 
     public void SetAction(Action action)
     {
         Text.text = action.Name;
 
         _action = action;
+
+        _lastDate = -1;
+
+        Button.onClick.AddListener(_action.Use);
     }
 
     public void Remove()
