821ff3cb83afd5e02916ff8c3b0cbcf26f9aec3c
Zematus <jb_tamez@hotmail.com>
2021-02-15 23:06:26 -0800

    Made expand decision effect work

---
+++ Assets/Scripts/WorldEngine/Modding/Entities/Attributes/Polity Entity Attributes/AddCoreRegionAttribute.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class AddCoreRegionAttribute : EffectEntityAttribute
+{
+    private PolityEntity _polityEntity;
+
+    private readonly IValueExpression<IEntity> _targetRegionExp;
+
+    public AddCoreRegionAttribute(PolityEntity polityEntity, IExpression[] arguments)
+        : base(PolityEntity.AddCoreRegionAttributeId, polityEntity, arguments, 1)
+    {
+        _polityEntity = polityEntity;
+
+        _targetRegionExp = ValueExpressionBuilder.ValidateValueExpression<IEntity>(arguments[0]);
+    }
+
+    public override void Apply()
+    {
+        RegionEntity regionEntity = _targetRegionExp.Value as RegionEntity;
+
+        if (regionEntity == null)
+        {
+            throw new System.ArgumentException(
+                "transfer_influence: invalid target region: " +
+                "\n - expression: " + ToString() +
+                "\n - target region: " + _targetRegionExp.ToPartiallyEvaluatedString());
+        }
+
+        _polityEntity.Polity.AddCoreRegion(regionEntity.Region);
+    }
+}
