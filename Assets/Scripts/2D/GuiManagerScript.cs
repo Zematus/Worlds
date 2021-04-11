4b6b09938ccb03cb895380750d55e9d68864e664
Zematus <jb_tamez@hotmail.com>
2021-04-10 17:53:24 -0700

    fixed #250

--- Assets/Scripts/2D/GuiManagerScript.cs
+++ Assets/Scripts/2D/GuiManagerScript.cs
@@ -425,7 +425,8 @@ public class GuiManagerScript : MonoBehaviour
             //GenerateWorld(false, 1253572363);
             //GenerateWorld(false, 403265427);
             //GenerateWorld(false, 70275341);
-            GenerateWorld(false, 1788979703);
+            //GenerateWorld(false, 1788979703);
+            GenerateWorld(false, 405241319);
         }
         else
         {
