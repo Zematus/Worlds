581744eb7c6a0ab8065ed8e9d3d95547f595796e
Juan Bernardo Tamez Peña <jb_tamez@hotmail.com>
2022-05-08 23:56:37 -0700

    added increment/decrement and assign expressions

---
+++ Assets/Scripts/WorldEngine/Modding/Expressions/EffectExpressions/ValueAddExpression.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class ValueAddExpression : BinaryOpExpression, IEffectExpression
+{
+    private readonly IAssignableValueExpression<float> _targetValueExp;
+    private readonly IValueExpression<float> _sourceValueExp;
+
+    public ValueAddExpression(
+        IExpression expressionA, IExpression expressionB)
+        : base("=", expressionA, expressionB)
+    {
+        _targetValueExp =
+            AssignableValueExpressionBuilder.ValidateAssignableValueExpression<float>(expressionA);
+        _sourceValueExp =
+            ValueExpressionBuilder.ValidateValueExpression<float>(expressionB);
+    }
+
+    public IEffectTrigger Trigger { get; set; }
+
+    public void Apply()
+    {
+        _targetValueExp.Value += _sourceValueExp.Value;
+    }
+
+    public override string ToPartiallyEvaluatedString(int depth)
+    {
+        return
+            "(" + _expressionA.ToPartiallyEvaluatedString(0) +
+            " += " + _expressionB.ToPartiallyEvaluatedString(depth) + ")";
+    }
+}
